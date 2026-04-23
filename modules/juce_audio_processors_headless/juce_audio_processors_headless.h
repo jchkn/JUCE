0a7da05044aa3083f9721bcd0b3da0c675c914fe
armpro14 <armpro14@sat>
2026-04-23 12:44:03 +0200

    added fake module

---
+++ modules/juce_audio_processors_headless/juce_audio_processors_headless.h
@@ -0,0 +1,20 @@
+
+
+/*******************************************************************************
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:                 juce_audio_processors_headless
+  vendor:             juce
+  version:            7.0.7
+  name:               juce_audio_processors_headless
+  description:        This is a fake module, to be compatible with newer juce versions
+  website:            
+  license:            
+  dependencies:       
+  OSXFrameworks:      
+  iOSFrameworks:      
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
